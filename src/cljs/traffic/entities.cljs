230aa3ddeac2bad32d8f1e98231a84ac4a6471cc
Darrell Hamilton <darrell.noice@gmail.com>
2014-09-13 22:58:18 -0600

    only add components that are defined

--- src/cljs/traffic/entities.cljs
+++ src/cljs/traffic/entities.cljs
@@ -11,9 +11,10 @@
 
 (defn create-car
   [system opts]
-   (let [car (b/create-entity)]
-     (-> system
-         (b/add-entity car)
-         (b/add-component car (:position opts))
-         (b/add-component car (:motion opts))
-         (b/add-component car (c/->RenderFn (car-renderer "red"))))))
+   (let [car (b/create-entity)
+         s (-> system
+               (b/add-entity car)
+               (b/add-component car (c/->RenderFn (car-renderer "red"))))]
+     (reduce (fn [s k] (if-let [opt (get opts k)]
+                (b/add-component s car opt)
+                s)) s [:position :motion :acceleration])))

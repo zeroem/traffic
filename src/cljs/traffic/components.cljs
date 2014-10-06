959c8489e2b8b2f97f67c8cd95ba32224ecc4482
Darrell Hamilton <darrell.noice@gmail.com>
2014-10-05 23:11:47 -0600

    Origin based boxes, and hitboxes

--- src/cljs/traffic/components.cljs
+++ src/cljs/traffic/components.cljs
@@ -3,4 +3,42 @@
 (defrecord Velocity [d s])
 (defrecord Acceleration [f])
 (defrecord Origin [x y])
+(defrecord OriginBox [ox oy dx dy])
 (defrecord RenderFn [f])
+(defrecord Hitbox [^OriginBox b])
+
+
+(defn originating-box [dx dy]
+  (->OriginBox 0 0 dx dy))
+
+(defn surrounding-box [dx dy]
+  (->OriginBox (- (/ dx 2)) (- (/ dy 2)) dx dy))
+
+(defn bounding-points [^Origin o ^OriginBox b]
+  (let [ll {:x (+ (:x o) (:ox b))
+            :y (+ (:y o) (:oy b))}]
+    [ll
+     {:x (+ (:x ll) (:dx b))
+      :y (+ (:y ll) (:dy b))}]))
+
+(defn box-origin [^Origin o ^OriginBox b]
+  (->Origin (+ (:x o) (:ox b)) (+ (:y o) (:oy b))))
+
+(defn within? [^Origin o ^OriginBox b ^Origin p]
+  (let [{px :x py :y} p
+        [[lx by] [rx ty]] (bounding-points o b)
+        ]
+      (and
+       (>= px lx)
+       (<= px rx)
+       (>= py by)
+       (<= py ty))))
+
+(defn overlaps? [^Origin o1 ^OriginBox b1 ^Origin o2 ^OriginBox b2]
+  (let [[[lx1 by1] [rx1 ty1]] (bounding-points o1 b1)
+        [[lx2 by2] [rx2 ty2]] (bounding-points o2 b2)]
+    (and
+     (<= lx1 rx2)
+     (>= rx1 lx2)
+     (<= ly1 ty2)
+     (>= ty1 ly2))))
--- src/cljs/traffic/entities.cljs
+++ src/cljs/traffic/entities.cljs
@@ -5,15 +5,19 @@
 
 (defn car-renderer [color]
   (fn [context s e]
-    (let [p (b/get-component s e c/Origin)]
+    (let [o (b/get-component s e c/Origin)
+          hb (b/get-component s e c/Hitbox)
+          b (:b hb)
+          o (c/box-origin o b)]
       (aset context "fillStyle" color)
-      (.fillRect context (:x p) (:y p) 5 10))))
+      (.fillRect context (:x o) (:y o) (:dx b) (:dy b) 10))))
 
 (defn create-car
   [system opts]
   (let [car (b/create-entity)
         s (-> system
               (b/add-entity car)
+              (b/add-component car (c/->Hitbox (c/surrounding-box 5 10)))
               (b/add-component car (c/->RenderFn (car-renderer "red"))))]
     (reduce (fn [s k] (if-let [opt (get opts k)]
                         (b/add-component s car opt)
